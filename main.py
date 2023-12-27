8b2f267b82dff91069cd680ee773c8933e717e99
IDCartographer <57636294+IDCartographer@users.noreply.github.com>
2023-12-27 14:00:04 +0300

    Dockerfile red

--- main.py
+++ main.py
@@ -249,3 +249,4 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+#end
